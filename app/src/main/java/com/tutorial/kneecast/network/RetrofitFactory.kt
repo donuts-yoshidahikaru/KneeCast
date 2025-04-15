322bff60c552fbe5a255c56e0707a7393b02714f
yoshida.hikaru <yoshida.hikaru@Donuts-NM2778-2411.local>
2025-04-15 12:47:51 +0900

    feat(network): ネットワーク通信基盤を追加
    - OkHttpクライアントを生成する `HttpClientProvider.kt` を追加
    - Retrofitインスタンスを生成する `RetrofitFactory.kt` を追加

---
+++ app/src/main/java/com/tutorial/kneecast/network/RetrofitFactory.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+fun createRetrofitInstance(baseUrl: String): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(createOkHttpClient())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
