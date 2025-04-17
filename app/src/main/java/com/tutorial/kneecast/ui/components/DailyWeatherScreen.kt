d2202b8bdd1f23dd9fac112f76cb2acb170ba930
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-04-17 16:21:27 +0900

    feat(ui): DailyWeatherScreenコンポーネントを新規作成
    - DailyWeatherScreen関数を追加し、DailyWeatherデータを表示するためのLazyColumnを実装
    - 各日付の天気情報をDailyWeatherCardコンポーネントを使用して表示

---
+++ app/src/main/java/com/tutorial/kneecast/ui/components/DailyWeatherScreen.kt
@@ -0,0 +1,35 @@
+package com.tutorial.kneecast.ui.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tutorial.kneecast.data.model.DailyWeather
+import com.tutorial.kneecast.ui.mapper.toUiModel
+
+@Composable
+fun DailyWeatherScreen(
+    dailyWeather: DailyWeather
+) {
+    val uiModels = dailyWeather.data.map { it.toUiModel() }
+    
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 8.dp),
+        contentPadding = androidx.compose.foundation.layout.PaddingValues(horizontal = 16.dp)
+    ) {
+        items(uiModels) { uiModel ->
+            DailyWeatherCard(
+                uiModel = uiModel,
+                iconSize = 96.dp,
+                fontSizeTemp = 24.sp
+            )
+        }
+    }
+}
