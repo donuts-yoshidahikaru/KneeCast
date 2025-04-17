15c3f795718a69dd6311d0049a805b9f5b5e24dc
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-04-17 16:56:52 +0900

    feat(data): GeocoderResponseを住所検索用にパラメータを追加 - ResultInfoデータクラスを追加し、 - FeatureにNameプロパティを追加

--- app/src/main/java/com/tutorial/kneecast/data/model/GeocoderResponse.kt
+++ app/src/main/java/com/tutorial/kneecast/data/model/GeocoderResponse.kt
@@ -1,11 +1,16 @@
 package com.tutorial.kneecast.data.model
 
 data class GeocoderResponse(
+    val ResultInfo: ResultInfo,
     val Feature: List<Feature>
 )
 
+data class ResultInfo(
+    val count: Int
+)
 data class Feature(
-    val Geometry: Geometry
+    val Geometry: Geometry,
+    val Name: String
 )
 
 data class Geometry(
